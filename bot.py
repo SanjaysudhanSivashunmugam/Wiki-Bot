ed75b97d0933d17cbbed848283a9dbe94f582cce
SanjaysudhanSivashunmugam <sanjaysudhan.sivashunmugam@gmail.com>
2024-11-22 21:04:40 +0530

    fixed bug1

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -24,5 +25,7 @@ def bot():
     else:
         return jsonify({"text": "Please ask a valid question."})
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
